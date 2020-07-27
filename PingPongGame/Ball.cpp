8b075ba4a0abeb1d4e492e789125dba7cb9bae1d
mstva <mustafaabdo468@gmail.com>
2020-07-27 21:44:42 +0200

    init ball velocity in init ball function

--- PingPongGame/Ball.cpp
+++ PingPongGame/Ball.cpp
@@ -12,6 +12,10 @@ void Ball::initBall()
 	// init ball position
 	mBallPos.x = (WIDTH - thickness) / 2;
 	mBallPos.y = (HIEGHT - thickness) / 2;
+
+	// init ball velocity
+	mBallVel.x = 200;
+	mBallVel.y = -200;
 }
 
 void Ball::showBall(SDL_Renderer* mRenderer)
