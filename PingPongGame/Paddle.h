b254e3bd8662bf72064c1ece516480df51b3f757
mstva <mustafaabdo468@gmail.com>
2020-07-27 20:33:54 +0200

    add public functions to paddle header

--- PingPongGame/Paddle.h
+++ PingPongGame/Paddle.h
@@ -9,5 +9,18 @@ class Paddle
 public:
 	// constractor
 	Paddle();
+
+	// public functions
+	void initPaddle();
+	void showPaddle(SDL_Renderer* mRenderer, std::string dir);
+	void movePaddle();
+	void updatePaddle(float deltaTime, std::string dir);
+
+private:
+	// private game logic variables
+	int mPaddleDirRight;
+	int mPaddleDirLeft;
+	Vector2 mPaddlePosRight;
+	Vector2 mPaddlePosLeft;
 };
 
