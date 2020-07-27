d60cb03a6ded45b89d24e0ef2e1dd666ee170870
mstva <mustafaabdo468@gmail.com>
2020-07-27 20:00:20 +0200

    add constants in constants header file

---
+++ PingPongGame/Constants.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#pragma once
+
+const int WIDTH = 800;
+const int HIEGHT = 600;
+const int thickness = 15;
+const int paddleH = 100;
+const int speed = 300;
+
+const struct Vector2
+{
+	int x;
+	int y;
+};
+
