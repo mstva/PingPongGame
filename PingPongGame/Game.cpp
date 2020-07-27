11c5a5b99988b16338b237b3e1bf189e8254f21b
mstva <mustafaabdo468@gmail.com>
2020-07-27 19:59:37 +0200

    add code to create new window

--- PingPongGame/Game.cpp
+++ PingPongGame/Game.cpp
@@ -10,7 +10,31 @@ Game::Game() :
 
 bool Game::initGame()
 {
-    return false;
+    // check SDL initialzation
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return false;
+    }
+
+    // create the window
+    mWindow = SDL_CreateWindow(
+        "Ping Pong",                       // title
+        SDL_WINDOWPOS_UNDEFINED,           // x position
+        SDL_WINDOWPOS_UNDEFINED,           // y position
+        WIDTH,                             // width
+        HIEGHT,                            // height
+        SDL_WINDOW_OPENGL                  // flag
+    );
+
+    // check window creating
+    if (mWindow == NULL)
+    {
+        SDL_Log("Unable to create SDL window: %s", SDL_GetError());
+        return false;
+    }
+
+    return true;
 }
 
 void Game::closeGame()
