dab09b62546058f5b3252f3ff0cc3650a6564642
mstva <mustafaabdo468@gmail.com>
2020-07-27 20:45:58 +0200

    declare functions in paddle class

---
+++ PingPongGame/Paddle.cpp
@@ -0,0 +1,21 @@
+#include "Paddle.h"
+
+Paddle::Paddle()
+{
+}
+
+void Paddle::initPaddle()
+{
+}
+
+void Paddle::showPaddle(SDL_Renderer* mRenderer, std::string dir)
+{
+}
+
+void Paddle::movePaddle()
+{
+}
+
+void Paddle::updatePaddle(float deltaTime, std::string dir)
+{
+}
